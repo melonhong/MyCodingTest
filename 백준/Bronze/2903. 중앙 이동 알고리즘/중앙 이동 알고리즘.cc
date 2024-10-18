e861823fa94828b29af92487ed7c983f9dc3ca22
melonhong <128899212+melonhong@users.noreply.github.com>
2024-10-19 01:02:01 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int n, r, d = 3;
+    cin >> n;
+    
+    r = pow(pow(2, n) + 1, 2);
+    
+    cout << r;
+    
+    return 0;
+}
