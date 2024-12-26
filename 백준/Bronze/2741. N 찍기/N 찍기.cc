7fdf9d7197e9e4be13760da456ece6f76d4c6718
melonhong <128899212+melonhong@users.noreply.github.com>
2024-12-26 13:46:19 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    scanf("%d", &a);
+    
+    for (int i = 1; i <= a; i++) {
+        printf("%d\n", i);
+    }
+    
+    return 0;
+}
