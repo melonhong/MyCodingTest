88beb3c2573b4560241a66f03a176f9e8f750211
melonhong <128899212+melonhong@users.noreply.github.com>
2024-12-26 21:04:09 +0900

    [Bronze II] Title: 음계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+bool comp(int a, int b) {
+    return b == a + 1;
+}
+
+int main() {
+    const int ARR_SIZE = 8;
+    int arr[ARR_SIZE];
+    char f;   // 'a' == ascending, 'd' == descending, 'm' == mixed
+    
+    for (int i = 0; i < ARR_SIZE; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    if (arr[0] == 1) {  // 상향 음계인지 확인
+        for (int i = 1; i < ARR_SIZE; i++) {
+            if (arr[i] == i + 1) {
+                f = 'a';
+            } else {
+                f = 'm';
+                break;
+            }
+        }
+    } else if (arr[0] == 8) {   // 하향 음계인지 확인
+        for (int i = 1; i < ARR_SIZE; i++) {
+            if (arr[i] == 8 - i) {
+                f = 'd';
+            } else {
+                f = 'm';
+                break;
+            }
+        }
+    }
+    
+    
+    if (f == 'a') {
+        printf("ascending");
+    } else if (f == 'd'){
+        printf("descending");
+    } else {
+        printf("mixed");
+    }
+    
+    return 0;
+}
